fdc6f781afcdbcf3d3fba0486ab7c6e7fae01cd1
Gabriel Vasconcelos <gabriels.v9@gmail.com>
2023-10-26 00:55:59 -0300

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody , baseURL) {
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const linkElelemts = dom.window.document.querySelectorAll('a')
+
+  for (const linkElelemt of linkElelemts) {
+    if(linkElelemt.href.slice(0,1) === '/') {
+      try {
+        const urlObject = new URL(`${baseURL}${linkElelemt.href}`)
+        urls.push(urlObject.href)
+      } catch(err){
+          console.log(`error with relative url: ${linkElelemt.href}`)
+      }
+    } else {
+        try {
+          const urlObject = new URL(linkElelemt.href)
+          urls.push(urlObject.href)
+        } catch(err){
+            console.log(`error with absolute url: ${err.message}`)
+        }
+      }
+  }
+  return urls
+}
+
+function getURLsFromPage(page) {
+  const urls = page.$$eval('a', as => as.map(a => a.href))
+  return urls
+}
+
 function normalizeUrl(url) {
   const urlObject = new URL(url)
   const hostPath = `${urlObject.hostname}${urlObject.pathname}`
@@ -10,5 +42,6 @@ function normalizeUrl(url) {
 }
 
 module.exports = {
-  normalizeUrl
+  normalizeUrl,
+  getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeUrl } = require('./crawl')
+const { normalizeUrl, getURLsFromHTML } = require('./crawl')
 const { test, expect } = require('@jest/globals')
 
 //https://asuratoon.com/
@@ -29,5 +29,63 @@ test('normalizeUrl strip http', () => {
     const input = 'http://ASURATOON.com/path/'
     const actual = normalizeUrl(input)
     const expected = 'asuratoon.com/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://asuratoon.com/path/">link</a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://asuratoon.com/path/"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://asuratoon.com/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">link</a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://asuratoon.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://asuratoon.com/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://asuratoon.com/path1/">link1</a>
+
+            <a href="/path2/">link2</a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://asuratoon.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://asuratoon.com/path1/", "https://asuratoon.com/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="invalid">Invalid URL</a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://asuratoon.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
